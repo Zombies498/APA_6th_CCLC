30051f31ede8ad46ec03bbd77ee0a2b3fcfb299a
User <User@DESKTOP-2NCKPFV>
2020-02-07 21:06:38 +0800

    change01

--- APA/src/NamePrint/DataProcess.java
+++ APA/src/NamePrint/DataProcess.java
@@ -1,14 +1,6 @@
 package NamePrint;
 
 public class DataProcess {
-//	public static void main(String[] args) {
-//		int[] first_next_split = new int[10];
-//		first_next_split = resetArray(first_next_split);
-//		
-//		for (int i = 0; i < first_next_split.length; i++) {
-//			System.out.println(first_next_split[i]);
-//		}
-//	}
 	public static int[] resetArray01(int[] a) {
 		for (int i = 0; i < a.length; i++) {
 			a[i] = 1; 
